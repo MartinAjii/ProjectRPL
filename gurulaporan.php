a4325493ad29c30e6d118ca64f3e5c822347ff0e
MartinAjii <martin.aji789@gmail.com>
2025-05-28 23:30:33 +0700

    tambah dikit

--- gurulaporan.php
+++ gurulaporan.php
@@ -6,24 +6,6 @@ session_start();
 if (!isset($_SESSION['inputUsm'])) {
     header("Location: login.php");
 }
-
-$username = $_SESSION['inputUsm'];
-$query = mysqli_query($conn, "SELECT * FROM akun WHERE usm = '$username'");
-
-if (mysqli_num_rows($query) > 0) {
-    $data = mysqli_fetch_assoc($query);
-    $id_guru = $data['id'];
-    $query_guru = mysqli_query($conn, "SELECT * FROM guru WHERE id_guru = '$id_guru'");
-
-    if (mysqli_num_rows($query_guru) > 0) {
-        $data_guru = mysqli_fetch_assoc($query_guru);
-    } else {
-        $data_guru = null;
-    }
-} else {
-    echo "Data pengguna tidak ditemukan.";
-    exit();
-}
 ?>
 
 <!DOCTYPE html>
@@ -39,18 +21,12 @@ if (mysqli_num_rows($query) > 0) {
             <a class="profile" href="profilguru.php">
                 <img src="logo.png" alt="Logo" class="pflogo">
             </a>
-            <img src="logo.png" alt="Logo" class="mainlogo">
-            <?php if ($data_guru): ?>
-            <p><strong>Nama:</strong> <?= $data_guru['nama_guru']?></p>
-            <p><strong>NIP:</strong> <?= $data_guru['nip_guru']?></p>
-            <?php endif; ?>
-            
             <nav class="navbar sticky-bottom navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link active" href="gurumenu.php">
-                            <img src="asset/VectorB1.png" alt="Logo" class="logo">
+                        <a class="nav-link" href="gurumenu.php">
+                            <img src="asset/Vector1.png" alt="Logo" class="logo">
                         </a>
                         <a class="nav-link" href="guruinput.php">
                             <img src="asset/Vector2.png" alt="Logo" class="logo">
@@ -61,8 +37,8 @@ if (mysqli_num_rows($query) > 0) {
                         <a class="nav-link" href="guruliatlab.php">
                             <img src="asset/Vector4.png" alt="Logo" class="logo">
                         </a>
-                        <a class="nav-link" href="gurulaporan.php">
-                            <img src="asset/Vector5.png" alt="Logo" class="logo">
+                        <a class="nav-link active" href="gurulaporan.php">
+                            <img src="asset/VectorB5.png" alt="Logo" class="logo">
                         </a>
                     </div>
                     </div>
--- profilguru.php
+++ profilguru.php
@@ -36,38 +36,18 @@ if (mysqli_num_rows($query) > 0) {
 <body>
     <div class="container col-lg-4 mt-5">
         <div class="card mb-3">
-            <a class="profile" href="profilguru.php">
+            <a class="profile" href="gurumenu.php">
                 <img src="logo.png" alt="Logo" class="pflogo">
             </a>
-            <img src="logo.png" alt="Logo" class="mainlogo">
+            <h2>Detail Pengguna</h2>
             <?php if ($data_guru): ?>
             <p><strong>Nama:</strong> <?= $data_guru['nama_guru']?></p>
+            <p><strong>E-mail:</strong> <?= $data_guru['email_guru']?></p>
+            <p><strong>Mapel:</strong> <?= $data_guru['mapel_guru']?></p>
+            <p><strong>Username:</strong> <?= $data_guru['usm_guru']?></p>
             <p><strong>NIP:</strong> <?= $data_guru['nip_guru']?></p>
             <?php endif; ?>
-            
-            <nav class="navbar sticky-bottom navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" href="gurumenu.php">
-                            <img src="asset/VectorB1.png" alt="Logo" class="logo">
-                        </a>
-                        <a class="nav-link" href="guruinput.php">
-                            <img src="asset/Vector2.png" alt="Logo" class="logo">
-                        </a>
-                        <a class="nav-link" href="guruliatjadwal.php">
-                            <img src="asset/Vector3.png" alt="Logo" class="logo">
-                        </a>
-                        <a class="nav-link" href="guruliatlab.php">
-                            <img src="asset/Vector4.png" alt="Logo" class="logo">
-                        </a>
-                        <a class="nav-link" href="gurulaporan.php">
-                            <img src="asset/Vector5.png" alt="Logo" class="logo">
-                        </a>
-                    </div>
-                    </div>
-                </div>
-            </nav>
+            <a href="proses.php?logout=true" class="btn btn-danger">Logout</a>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
